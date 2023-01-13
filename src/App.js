39cb37fa6a72f739737e8aa17119a7a36b8d04e2
Hassan <kkromah12@gmail.com>
2023-01-14 00:22:02 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Welcome } from "./Welcome";
+
+export class App extends React.Component{
+
+
+   render(){
+
+      return(
+         <div>
+            <Welcome age ="19"/>
+         </div> 
+      )
+   }
+}
