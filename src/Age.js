9c8869b4d2cb932cabab141919e9d1e49ef7f7ea
Hassan <kkromah12@gmail.com>
2023-01-14 00:10:58 +0300

    done

---
+++ src/Age.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class Age extends React.Component{
+
+
+   render(){
+
+      return(
+         <div><p>Your age is {this.props.your_age}</p></div>
+      )
+   }
+}
---
+++ src/Welcome.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Age } from "./Age";
+
+export class Welcome extends React.Component{
+
+
+   render(){
+      return(
+         <div>
+            {this.props.age? <Age your_age={this.props.age}/>: ' '}
+         </div>
+      )
+   }
+}
