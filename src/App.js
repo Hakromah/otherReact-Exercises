250a72f5adc1391d1bb12c254ecefc1d9ebb8361
Hassan <kkromah12@gmail.com>
2023-01-26 15:48:03 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ClickTraker } from './ClickTraker'
+
+
+
+
+
+
+export default function App() {
+   return (
+     <div>
+     <ClickTraker/>
+     </div>
+   )
+ }
