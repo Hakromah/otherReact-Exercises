708efe9c607da6c8ac8d5c9ddd171a67f37eebc3
Hassan <kkromah12@gmail.com>
2023-01-26 15:42:11 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ClickCounter } from './ClickCounter'
+
+
+
+
+
+
+export default function App() {
+   return (
+     <div>
+     <ClickCounter/>
+     </div>
+   )
+ }
