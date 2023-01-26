b12dbc5484966c9b079282edd9a87c1732aec2a3
Hassan <kkromah12@gmail.com>
2023-01-26 15:22:51 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import SumDefault from './SumDefault'
+import Sum from './Sum'
+
 
 
 
 export default function App() {
    return (
      <div>
-     <SumDefault/>
+      <Sum numbers= {[5, 6, 10, 74, 10]}/>
      </div>
    )
  }
--- src/Sum.js
+++ src/Sum.js
@@ -2,16 +2,13 @@ import React from 'react';
 
 // we set default value in an arry box if the props value is not given.
 // because we use "reduce " method to the get the sum of an arry. 
-export default function SumDefault({numbers}) {
+export default function Sum(props) {
 
 	
 	return (
 		
 		<div>
-			Sum <h1>{numbers.reduce((prev, curr) => prev + curr)}</h1>
+			Sum <h1>{props.numbers.reduce((prev, curr) => prev + curr)}</h1>
 		</div>
 	);
-}
-SumDefault.defaultProps ={
-	numbers: [15]
 }
