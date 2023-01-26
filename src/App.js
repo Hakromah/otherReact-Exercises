f531555115802ccc3d3263390d32bc0570093eab
Hassan <kkromah12@gmail.com>
2023-01-26 15:14:35 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Sum from './Sum'
+import SumDefault from './SumDefault'
 
 
 
 export default function App() {
    return (
      <div>
-     <Sum numbers= {[5, 6, 3, 8]}/>
+     <SumDefault/>
      </div>
    )
  }
