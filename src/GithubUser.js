7051d9bf6f543b08fc61a973744d92012cc178e8
Hassan <kkromah12@gmail.com>
2023-01-27 15:50:57 +0300

    parcially done

---
+++ src/GithubUser.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+
+
+function useGithubUser({username}) {
+	const [data, setData] = useState([]);
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(null);
+
+	async function fetchData() {
+		try {
+			const getData = await fetch(
+				`https://api.github.com/users/${username}`
+			);
+			const json = await getData.json();
+			setData(json);
+			console.log(json);
+		} catch (error) {
+			setError(error);
+		} finally {
+			setLoading(false);
+		}
+	}
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	return {
+		data,
+		error,
+		loading,
+	}
+}
+
+export function GithubUser() {
+	const {data} = useGithubUser([])
+	const {error} = useGithubUser(false)
+	const {loading} = useGithubUser(null)
+
+	return (
+		<div>
+			{error && <h2>Data not fund</h2>}
+			{loading && <h2>Data is loading...</h2>}
+			<h1 key={data.id}>{data.name}</h1>
+		</div>
+	);
+};
