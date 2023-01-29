6184d4827abd5c75470a2615a9620a6da4d77e2e
Hassan <kkromah12@gmail.com>
2023-01-29 18:23:15 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Counter } from './Counter';
+import ShowGithubUser from './ShowGithubUser';
+import Welcome from './Welcome';
+
+function App() {
+	return (
+		<div>
+			<Link to="/">Welcome</Link>
+			<br />
+			<Link to="/counter">Counter</Link>
+			<br />
+			<Link to="/showGithubUser">Show Github User</Link>
+			<Routes>
+				<Route path="/" element={<Welcome name="Hassan" />} />
+				<Route path="/counter" element={<Counter />} />
+				<Route path="/:username" element={<ShowGithubUser />} />
+				<Route
+					path="*"
+					element={
+						<div>
+							<p>
+								<h1>404</h1>
+								Page not found
+							</p>
+						</div>
+					}
+				/>
+			</Routes>
+		</div>
+	);
+}
+
+export default App;
