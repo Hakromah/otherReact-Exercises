0ba8a137a5cfceb82d09ad58b61bfb04729ffa16
Hassan <kkromah12@gmail.com>
2023-01-31 01:51:24 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,38 +1,28 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import { Counter } from './Counter';
-import { GithubUserList } from './GithubUserList';
+import GithubUserList from './GithubUserList';
 import ShowGithubUser from './ShowGithubUser';
 import Welcome from './Welcome';
 
-function App() {
+export default function App() {
 	return (
 		<div>
 			<Link to="/">Welcome</Link>
 			<br />
 			<Link to="/counter">Counter</Link>
 			<br />
-			<Link to="/showGithubUser">Show Github User</Link>
+			<Link to="/ShowGithubUser">Show Github User</Link>
 			<Routes>
 				<Route path="/" element={<Welcome name="Hassan" />} />
 				<Route path="/counter" element={<Counter />} />
-				<Route path="/showGithubUser" element={<GithubUserList/>}>
+				<Route path="/ShowGithubUser" element={<GithubUserList/>}>
 					<Route path=':users' element={<ShowGithubUser/>}/>
 				</Route>
-				<Route
-					path="*"
-					element={
-						<div>
-							<p>
-								<h1>404</h1>
-								Page not found
-							</p>
-						</div>
-					}
-				/>
+				
 			</Routes>
 		</div>
 	);
 }
 
-export default App;
+
--- src/GithubUserList.js
+++ src/GithubUserList.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-export const GithubUserList = () => {
-  return (
-    <div>GithubUserList
-    </div>
-  )
+export default function GithubUserList() {
+	const [user, setUser] = useState([]);
+
+	useEffect(() => {
+		fetch(`https://api.github.com/users`)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				setUser(data);
+			});
+	}, []);
+
+	return (
+		<div>
+			{user.map((user, i) => (
+				<li key={i}>
+					{user.login}
+				</li>
+			))}
+		</div>
+	);
 }
