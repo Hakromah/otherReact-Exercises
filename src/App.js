94389268dc0bbf0e83c5e19a218cfbb43a661e77
Hassan <kkromah12@gmail.com>
2023-02-01 01:23:49 +0300

    edited

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from "react";
-import Login  from "./Login";
+import React from 'react';
+import Login from './Login';
 
-export class App extends React.Component{
-   
+export class App extends React.Component {
+	onLoginFunction = (a, b, c) => {
+		console.log(a, b, c);
+	};
 
-   render(){
-      
-      return(
-         <div>
-           <Login/>       
-         </div> 
-      )
-   }
+	render() {
+		return (
+			<div>
+				<Login onLogin={this.onLoginFunction} />
+			</div>
+		);
+	}
 }
--- src/Login.js
+++ src/Login.js
@@ -1,59 +1,71 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
 
 export class Login extends Component {
+	state = {
+		username: '',
+		password: '',
+		remember: false,
+		disabled: true,
+	};
 
-   state = {
-      username: '',
-      password: '',
-      remember: false,
-      disabled: true
-   }
+	handleOnChange = (evt) => {
+		const { name, value, type, checked } = evt.target;
+		this.setState({
+			[name]: type === 'checkbox' ? checked : value,
+		});
 
-   handleOnChange =(evt) => {
-      const {name, value, type, checked} = evt.target
-      this.setState({
-         [name] : type === 'checkbox' ? checked: value
-      })
+		if (this.state.username === '' || this.state.password === '') {
+			this.setState({
+				disabled: true,
+			});
+		} else {
+			this.setState({
+				disabled: false,
+			});
+		}
+	};
 
+	//!OnLogin function
 
-      if(this.state.username === '' || this.state.password ==='') {
-         this.setState({
-            disabled: true 
-         })
-      }else{
-         this.setState({
-            disabled: false
-         })
-      }
-   }
+	render() {
+		let { username, password, remember } = this.state;
+		return (
+			<div>
+				<label htmlFor="username">Username: </label>
+				<input
+					type="text"
+					name="username"
+					value={this.state.username}
+					onChange={this.handleOnChange}
+				/>
+				<br />
 
-   //!OnLogin function
-   onLogin=() => {
-      console.log(
-         this.state.username,
-         this.state.password,
-         this.state.remember
-      );
-   }
+				<label htmlFor="password">Password: </label>
+				<input
+					type="password"
+					name="password"
+					value={this.state.password}
+					onChange={this.handleOnChange}
+				/>
+				<br />
 
-
-
-  render() {
-    return (
-      <div>
-         <label htmlFor="username">Username: </label>
-         <input type="text" name="username" value={this.state.username} onChange={this.handleOnChange}/><br />
-
-         <label htmlFor="password">Password: </label>
-         <input type="password" name="password" value={this.state.password} onChange={this.handleOnChange}/><br />
-
-         <label htmlFor="checkbox">Remember: </label>
-         <input type="checkbox" name="remember" checked={this.state.remember} onChange={this.handleOnChange}/><br />
-         <button onClick={this.onLogin} disabled={this.state.disabled}>Login</button>
-      </div>
-    )
-  }
+				<label htmlFor="checkbox">Remember: </label>
+				<input
+					type="checkbox"
+					name="remember"
+					checked={this.state.remember}
+					onChange={this.handleOnChange}
+				/>
+				<br />
+				<button
+					onClick={() => this.props.onLogin(username, password, remember)}
+					disabled={this.state.disabled}
+				>
+					Login
+				</button>
+			</div>
+		);
+	}
 }
 
-export default Login
+export default Login;
