78bf665ee4e96f3755b7a8cf916aad1ded691d4d
Hassan <kkromah12@gmail.com>
2023-02-14 13:53:48 +0300

    updated

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-export default function GithubUser({ user}) {
+export default function GithubUser({ user }) {
 	const [userInfo, setUserInfo] = useState();
 
 	useEffect(() => {
@@ -8,28 +8,13 @@ export default function GithubUser({ user}) {
 			.then((response) => response.json())
 			.then((data) => {
 				setUserInfo(data);
-				console.log(data)//! console log to check if it receives the data
 			});
-	}, [user])
-
-	useEffect(() => {
-		if (user.length > 0) {
-			localStorage.setItem('items', JSON.stringify(user));
-		}
 	}, [user]);
 
-	useEffect(() => {
-		let getData = JSON.parse(localStorage.getItem('items'));
-		if (getData) {
-			setUserInfo([getData]);
-		}
-	}, []);
 
 	return (
 		<div>
-			<h2>
-			{userInfo && userInfo.name}
-		</h2>
+			<h2>{userInfo && userInfo.name}</h2>
 		</div>
 	);
 }
